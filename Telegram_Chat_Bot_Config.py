8fecaf462a17a8d0d22332404190e55b653bf4db
Iskander5 <124901399+Iskander5@users.noreply.github.com>
2023-05-02 21:38:45 +0300

    Add files via upload

---
+++ Telegram_Chat_Bot_Config.py
@@ -0,0 +1,10 @@
+TOKEN = '6051947870:AAFSQgTUX98fdobrSVhwLhNEIjrXQ3BlCIM'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'фунт': 'GBR',
+    'йена': 'JPY',
+    'юань': 'CNY',
+}
